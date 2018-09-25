417bdcd398bdd0430773f76603aa077951ef586c
grammophone <emmanuel.liossis@gmail.com>
2018-09-25 05:08:02 +0300

    Add null logger implementation.

---
+++ Null/NullLogger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grammophone.Logging.Null
+{
+	/// <summary>
+	/// An <see cref="ILogger"/> implementation doing nothing.
+	/// </summary>
+	public class NullLogger : ILogger
+	{
+		/// <summary>
+		/// Does nothing.
+		/// </summary>
+		public void Log(LogLevel logLevel, string message, params object[] arguments)
+		{
+		}
+
+		/// <summary>
+		/// Does nothing.
+		/// </summary>
+		public void Log(LogLevel logLevel, Exception exception, string message, params object[] arguments)
+		{
+		}
+
+		/// <summary>
+		/// Does nothing.
+		/// </summary>
+		public void Log(LogLevel logLevel, IFormatProvider formatProvider, string message, params object[] arguments)
+		{
+		}
+
+		/// <summary>
+		/// Does nothing.
+		/// </summary>
+		public void Log(LogLevel logLevel, Exception exception, IFormatProvider formatProvider, string message, params object[] arguments)
+		{
+		}
+	}
+}
---
+++ Null/NullLoggerProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grammophone.Logging.Null
+{
+	/// <summary>
+	/// Provider for <see cref="NullLogger"/>.
+	/// </summary>
+	public class NullLoggerProvider : ILoggerProvider
+	{
+		/// <summary>
+		/// Creates a <see cref="NullLogger"/> irrespective of <paramref name="loggerName"/>.
+		/// </summary>
+		/// <returns>Returns a new <see cref="NullLogger"/>.</returns>
+		public ILogger GetLogger(string loggerName) => new NullLogger();
+	}
+}
