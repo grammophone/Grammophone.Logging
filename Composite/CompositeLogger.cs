31184bb1bffeb13d9dddae275e80edc49f0c0913
grammophone <emmanuel.liossis@gmail.com>
2018-09-26 22:02:53 +0300

    Add stock composite logger.

---
+++ Composite/CompositeLogger.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grammophone.Logging.Composite
+{
+	/// <summary>
+	/// A logger which composes several loggers into one.
+	/// </summary>
+	public class CompositeLogger : ILogger
+	{
+		#region Private fields
+
+		private readonly ILogger[] aggregatedLoggers;
+
+		#endregion
+
+		#region Construction
+
+		internal CompositeLogger(ILogger[] aggregatedLoggers)
+		{
+			if (aggregatedLoggers == null) throw new ArgumentNullException(nameof(aggregatedLoggers));
+
+			this.aggregatedLoggers = aggregatedLoggers;
+		}
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// The aggregated loggers.
+		/// </summary>
+		public IReadOnlyCollection<ILogger> AggregatedLoggers { get; }
+
+		#endregion
+
+		#region Public methods
+
+		/// <summary>
+		/// Write an entry to the <see cref="AggregatedLoggers"/>.
+		/// </summary>
+		/// <param name="logLevel">The severity level of the log entry.</param>
+		/// <param name="message">
+		/// The message for the log entry, optionally in a <see cref="String.Format(string, object[])"/>
+		/// form when there are <paramref name="arguments"/>.</param>
+		/// <param name="arguments">Optional arguments to format the <paramref name="message"/>.
+		/// </param>
+		public void Log(LogLevel logLevel, string message, params object[] arguments)
+		{
+			for (int i = 0; i < aggregatedLoggers.Length; i++)
+			{
+				aggregatedLoggers[i].Log(logLevel, message, arguments);
+			}
+		}
+
+		/// <summary>
+		/// Write an entry to the <see cref="AggregatedLoggers"/>.
+		/// </summary>
+		/// <param name="logLevel">The severity level of the log entry.</param>
+		/// <param name="exception">The exception to record in the log entry.</param>
+		/// <param name="message">
+		/// The message for the log entry, optionally in a <see cref="String.Format(string, object[])"/>
+		/// form when there are <paramref name="arguments"/>.
+		/// </param>
+		/// <param name="arguments">Optional arguments to format the <paramref name="message"/>.</param>
+		public void Log(LogLevel logLevel, Exception exception, string message, params object[] arguments)
+		{
+			for (int i = 0; i < aggregatedLoggers.Length; i++)
+			{
+				aggregatedLoggers[i].Log(logLevel, exception, message, arguments);
+			}
+		}
+
+		/// <summary>
+		/// Write an entry to the <see cref="AggregatedLoggers"/>.
+		/// </summary>
+		/// <param name="logLevel">The severity level of the log entry.</param>
+		/// <param name="formatProvider">The formatter to use for the <paramref name="message"/>.</param>
+		/// <param name="message">
+		/// The message for the log entry, in a <see cref="String.Format(IFormatProvider, string, object[])"/> form.
+		/// </param>
+		/// <param name="arguments">Optional arguments to format the <paramref name="message"/>.</param>
+		public void Log(LogLevel logLevel, IFormatProvider formatProvider, string message, params object[] arguments)
+		{
+			for (int i = 0; i < aggregatedLoggers.Length; i++)
+			{
+				aggregatedLoggers[i].Log(logLevel, formatProvider, message, arguments);
+			}
+		}
+
+		/// <summary>
+		/// Write an entry to the <see cref="AggregatedLoggers"/>.
+		/// </summary>
+		/// <param name="logLevel">The severity level of the log entry.</param>
+		/// <param name="exception">The exception to record in the log entry.</param>
+		/// <param name="formatProvider">The formatter to use for the <paramref name="message"/>.</param>
+		/// <param name="message">
+		/// The message for the log entry, in a <see cref="String.Format(IFormatProvider, string, object[])"/> form.
+		/// </param>
+		/// <param name="arguments">Optional arguments to format the <paramref name="message"/>.</param>
+		public void Log(LogLevel logLevel, Exception exception, IFormatProvider formatProvider, string message, params object[] arguments)
+		{
+			for (int i = 0; i < aggregatedLoggers.Length; i++)
+			{
+				aggregatedLoggers[i].Log(logLevel, exception, formatProvider, message, arguments);
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ Composite/CompositeLoggerProvider.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grammophone.Logging.Composite
+{
+	/// <summary>
+	/// A logger logger provider which aggregates other logger providers
+	/// into producing <see cref="CompositeLogger"/>.
+	/// </summary>
+	public class CompositeLoggerProvider : ILoggerProvider
+	{
+		#region Private fields
+
+		private readonly ILoggerProvider[] aggregatedLogerProviders;
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// The aggregated logger providers.
+		/// </summary>
+		public IReadOnlyCollection<ILoggerProvider> AggregatedLoggerProviders => aggregatedLogerProviders;
+
+		#endregion
+
+		#region Construction
+
+		/// <summary>
+		/// Create.
+		/// </summary>
+		/// <param name="aggregatedLogerProviders">The aggregated logger providers.</param>
+		public CompositeLoggerProvider(ILoggerProvider[] aggregatedLogerProviders)
+		{
+			if (aggregatedLogerProviders == null) throw new ArgumentNullException(nameof(aggregatedLogerProviders));
+
+			this.aggregatedLogerProviders = aggregatedLogerProviders;
+		}
+
+		#endregion
+
+		#region Public methods
+
+		/// <summary>
+		/// Create a <see cref="CompositeLogger"/> by invoking <see cref="ILoggerProvider.CreateLogger(string)"/>
+		/// on each of the provider in <see cref="AggregatedLoggerProviders"/>.
+		/// </summary>
+		/// <param name="loggerName">The name of the logger requested by each provider in <see cref="AggregatedLoggerProviders"/>.</param>
+		/// <returns>Returns the requested <see cref="CompositeLogger"/>.</returns>
+		public ILogger CreateLogger(string loggerName)
+		{
+			if (loggerName == null) throw new ArgumentNullException(nameof(loggerName));
+
+			var aggregatedLoggers = new ILogger[aggregatedLogerProviders.Length];
+
+			for (int i = 0; i < aggregatedLogerProviders.Length; i++)
+			{
+				aggregatedLoggers[i] = aggregatedLogerProviders[i].CreateLogger(loggerName);
+			}
+
+			return new CompositeLogger(aggregatedLoggers);
+		}
+
+		#endregion
+	}
+}
